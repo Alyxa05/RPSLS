b98c5a30626c7a2613866b707d0e18b1a03c622f
Alyx <chiefdeland@gmail.com>
2026-03-26 15:25:27 -0400

    New logic and better html nesting

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Javascript for Assignment 3
 
 
+
 function showChars() {
     document.getElementById('name').classList.remove('active');
     document.getElementById('name').classList.add('inactive');
@@ -17,3 +18,29 @@ function showGame() {
     document.getElementById('game').classList.add('active');
 }
 
+function validateName() {
+    const nameInput = document.getElementById('nameinput');
+    if (nameInput.value.trim() === "") {
+        document.getElementById('errorMessage').textContent = "Please enter your name.";
+        return false;
+    }
+    document.getElementById('errorMessage').textContent = "";
+    showChars();
+    return true;
+}
+
+function charSelect() {
+    const characterSelect = document.getElementById('char-selection')
+
+    if (characterSelect.value === "") {
+        document.getElementById('errorMessage').textContent = "Please select a character";
+        return false;
+    }
+    document.getElementById('errorMessage').textContent = "";
+    showGame();
+    return true;
+}
+
+function validateForm() {
+     
+}
