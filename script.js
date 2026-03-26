879b32e5ff9ee928c6c2dc656e592c9923128944
Alyx <chiefdeland@gmail.com>
2026-03-26 12:29:20 -0400

    Characters, show hide func

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // Javascript for Assignment 3
+
+
+function showChars() {
+    document.getElementById('name').classList.remove('active');
+    document.getElementById('name').classList.add('inactive');
+
+    document.getElementById('characters').classList.remove('inactive');
+    document.getElementById('characters').classList.add('active');
+}
+
+function showGame() {
+    document.getElementById('characters').classList.remove('active');
+    document.getElementById('characters').classList.add('inactive');
+
+    document.getElementById('game').classList.remove('inactive');
+    document.getElementById('game').classList.add('active');
+}
+
